753e92ffacf4fbdf00959c11ecd46c118a41e572
Jarom Loveridge <jaroml@gradecam.com>
2015-08-03 14:10:42 -0600

    Add router loader.

---
+++ routers/index.js
@@ -0,0 +1,14 @@
+/* vim: set softtabstop=4 ts=4 sw=4 expandtab tw=120 syntax=javascript: */
+/* jshint node:true, unused:true */
+'use strict';
+
+var routers = module.exports = {};
+
+var fs = require('fs');
+
+fs.readdirSync(__dirname).forEach(function(filename) {
+    if (filename !== 'index.js' && filename.substr(-3) === '.js') {
+        var name = filename.substr(0, filename.length - 3);
+        routers[name] = require('./' + filename);
+    }
+});
